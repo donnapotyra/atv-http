1d67e31ee0c0a47f7c7537d850bf389f2b944d53
Potyra <potyraspires@gmail.com>
2022-10-02 16:42:41 -0300

    primeira versão

--- index.js
+++ index.js
@@ -9,19 +9,27 @@ const server = http.createServer((requisicao, resposta) => {
   // a "requisicao" possui um atributo chamado "method"
   // com ela é possível saber qual o método foi feito a requisição
   // com isso é feito a verificação para saber o que fazer
+  //
   if (requisicao.method === "GET") {
     
     resposta.write(`<h1>Caixa do mercado</h1>
-    <h2> bah gurizes</h2>`);
+    <h2> 4 Tomates</h2> 
+    <h3> 2 Batatas</h3>
+    <h4> 5 Cebola</h4>
+    <h4> 1kg de arroz</h4>`);
   } else if (requisicao.method === "POST") {
     
-    resposta.write("<h1>Adicionar em seu carrinho</h1>");
+    resposta.write(`<h1>Adicionar em seu carrinho</h1>
+    <h2> 1kg de feijão</h2>
+    <h3> Ovos</h3>`);
   } else if (requisicao.method === "PUT") {
    
-    resposta.write("<h1>Adicionar mesmo produto 2 vezes</h1>");
+    resposta.write(`<h1>Adicionar mesmo produto 2 vezes</h1>
+    <h2> Sal de churrasco</h2>`);
   } else if (requisicao.method === "DELETE") {
     
-    resposta.write("<h1>Remover um produto</h1>");
+    resposta.write(`<h1>Remover um produto</h1>
+    <h2> Sal de churrasco</h2>`);
   }
 
   // finaliza 
